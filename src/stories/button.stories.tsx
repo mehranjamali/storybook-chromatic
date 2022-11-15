aad352b9645d679aa706493e12fa19273d64b34e
mehranJamali <mehranjamali117@gmail.com>
2022-11-16 00:09:33 +0330

    test 1

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -9,7 +9,7 @@ export default {
 const Template: ComponentStory<typeof Button> = (args: any) => <Button {...args} />;
 
 export const defaultButton = Template.bind({});
-defaultButton.args = { label: "my name is button", buttonClassName: "px-3 py-2 bg-sky-400 rounded-lg text-sm" };
+defaultButton.args = { label: "Button", buttonClassName: "px-3 py-2 bg-sky-400 rounded-lg text-sm" };
 
 export const redButton = Template.bind({});
 redButton.args = { label: "Button", buttonClassName: "px-3 py-2 bg-red-500 rounded-lg text-sm" };
